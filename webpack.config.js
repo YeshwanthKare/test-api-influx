d3d886d5934f5e9888201525ea0c84feb103213d
Erzhan-Temir <admin@banoffee.kg>
2021-05-09 17:13:27 +0200

    2. build: webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -7,30 +7,11 @@ module.exports = {
     filename: `bundle.js`,
     path: path.join(__dirname, `public`),
   },
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: []
-      },
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: `babel-loader`,
-          options: {
-            presets: [`@babel/preset-env`],
-            plugins: [
-              `@babel/plugin-transform-runtime`
-            ]
-          }
-        }
-      }
-    ]
-  },
   devtool: `source-map`,
   devServer: {
     contentBase: path.join(__dirname, `public`),
+    publicPath: `http://localhost:8080/`,
+    hot: true,
     watchContentBase: true,
     liveReload: true,
     historyApiFallback: true
